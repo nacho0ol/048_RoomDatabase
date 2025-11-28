6e5d73bdaca2e2b7f2dcac1fecf51104862cf1f8
nachool <nachma.olivia1394@gmail.com>
2025-11-28 19:48:19 +0700

    menambah class EntryViewModel dan diisi dengan variabel uiStateSiswa, function validasiinput, updateuistate, dan savesiswa

--- app/src/main/java/com/example/myroomsatu/viewmodel/EntryViewModel.kt
+++ app/src/main/java/com/example/myroomsatu/viewmodel/EntryViewModel.kt
@@ -1,2 +1,34 @@
 package com.example.myroomsatu.viewmodel
 
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.ViewModel
+import com.example.myroomsatu.repositori.RepositoriSiswa
+import com.example.myroomsatu.room.Siswa
+
+class EntryViewModel(private val repositoriSiswa: RepositoriSiswa): ViewModel() {
+    /**
+     * Berisi status Siswa saat ini
+     */
+    var uiStateSiswa by mutableStateOf(UIStateSiswa())
+        private set
+
+    /* Fungsi untuk memvalidasi input */
+    private fun validasiInput(uiState: DetailSiswa = uiStateSiswa.detailSiswa): Boolean {
+        return with(uiState) {
+            nama.isNotBlank() && alamat.isNotBlank() && telpon.isNotBlank()
+        }
+    }
+
+    fun updateUiState(detailSiswa: DetailSiswa) {
+        uiStateSiswa =
+            UIStateSiswa(detailSiswa = detailSiswa, isEntryValid = validasiInput(detailSiswa))
+    }
+
+    /* Fungsi untuk menyimpan data yang di-entry */
+    suspend fun saveSiswa() {
+        if (validasiInput()) {
+            repositoriSiswa.insertSiswa(uiStateSiswa.detailSiswa.toSiswa())
+        }
+    }
+}
+
