0ff2cfeb62c2fecb5d5edeee8f330559cc92a914
nachool <nachma.olivia1394@gmail.com>
2025-11-28 20:19:30 +0700

    menambah object DestinasiHome: route dan titleRes

--- app/src/main/java/com/example/myroomsatu/view/route/DestinasiHome.kt
+++ app/src/main/java/com/example/myroomsatu/view/route/DestinasiHome.kt
@@ -1,4 +1,8 @@
 package com.example.myroomsatu.view.route
 
-object DestinasiHome {
+import com.example.myroomsatu.R
+
+object DestinasiHome : DestinasiNavigasi {
+    override val route = "home"
+    override val titleRes = R.string.app_name
 }
