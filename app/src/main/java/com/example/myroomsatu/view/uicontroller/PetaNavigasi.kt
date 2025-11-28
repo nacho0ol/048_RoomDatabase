64de82d4a6b5e9c1bc1f5c5e501104e69388072f
nachool <nachma.olivia1394@gmail.com>
2025-11-28 20:29:43 +0700

    mengganti nama route di navigasi

--- app/src/main/java/com/example/myroomsatu/view/uicontroller/PetaNavigasi.kt
+++ app/src/main/java/com/example/myroomsatu/view/uicontroller/PetaNavigasi.kt
@@ -1,2 +1,40 @@
 package com.example.myroomsatu.view.uicontroller
 
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.myroomsatu.view.route.DestinasiEntry
+import com.example.myroomsatu.view.route.DestinasiHome
+import com.example.myroomsatu.view.EntrySiswaScreen
+import com.example.myroomsatu.view.HomeScreen
+
+
+@Composable
+fun SiswaApp(navController: NavHostController = rememberNavController(),
+             modifier: Modifier = Modifier){
+    HostNavigasi(navController = navController)
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun HostNavigasi(
+    navController: NavHostController,
+    modifier: Modifier = Modifier
+){
+    NavHost(navController=navController, startDestination = DestinasiHome.route
+        , modifier = Modifier)
+    {
+        composable(DestinasiHome.route){
+            HomeScreen(
+                navigateToItemEntry = {navController.navigate(DestinasiEntry.route)},
+            )
+        }
+        composable(DestinasiEntry.route){
+            EntrySiswaScreen(navigateBack = { navController.popBackStack()})
+        }
+    }
+}
