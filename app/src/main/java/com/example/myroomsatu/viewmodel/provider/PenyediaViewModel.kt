c7475786baa5bff20a62661df04f2b6f5eb21578
nachool <nachma.olivia1394@gmail.com>
2025-11-28 19:53:29 +0700

    menambah object PenyediaViewModel dan funtion CreationExtras.aplikasiSiswa

--- app/src/main/java/com/example/myroomsatu/viewmodel/provider/PenyediaViewModel.kt
+++ app/src/main/java/com/example/myroomsatu/viewmodel/provider/PenyediaViewModel.kt
@@ -1,2 +1,28 @@
 package com.example.myroomsatu.viewmodel.provider
 
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewmodel.CreationExtras
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+import com.example.myroomsatu.repositori.AplikasiSiswa
+import com.example.myroomsatu.viewmodel.EntryViewModel
+import com.example.myroomsatu.viewmodel.HomeViewModel
+
+object PenyediaViewModel {
+    val Factory = viewModelFactory {
+        initializer {
+            HomeViewModel(aplikasiSiswa().container.repositoriSiswa)
+        }
+
+        initializer {
+            EntryViewModel(aplikasiSiswa().container.repositoriSiswa)
+        }
+    }
+}
+
+/**
+ * Fungsi ekstensi query untuk objek [Application] dan mengembalikan sebuah instance dari
+ * [AplikasiSiswa].
+ */
+fun CreationExtras.aplikasiSiswa(): AplikasiSiswa =
+    (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as AplikasiSiswa)
