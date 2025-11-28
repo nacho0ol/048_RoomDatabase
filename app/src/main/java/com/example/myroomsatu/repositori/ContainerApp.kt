3b6827a54d2fc6079d7f215cd8c3e2b3861eb13f
nachool <nachma.olivia1394@gmail.com>
2025-11-28 15:49:04 +0700

    menambah ContainerApp.kt

--- app/src/main/java/com/example/myroomsatu/repositori/ContainerApp.kt
+++ app/src/main/java/com/example/myroomsatu/repositori/ContainerApp.kt
@@ -1,2 +1,27 @@
 package com.example.myroomsatu.repositori
 
+import android.app.Application
+import android.content.Context
+import com.example.myroomsatu.room.DatabaseSiswa
+
+interface ContainerApp {
+    val repositorisiswa : RepositoriSiswa
+}
+
+class ContainerDataApp(private val context: Context):
+        ContainerApp {
+            override val repositorisiswa: RepositoriSiswa by lazy {
+                OfflineRepositorisiswa(
+                    DatabaseSiswa.getDatabase(context).siswaDao()
+                )
+            }
+        }
+
+class AplikasiSiswa : Application() {
+    lateinit var container: ContainerApp
+
+    override fun onCreate() {
+        super.onCreate()
+        container = ContainerDataApp(this)
+    }
+}
