f4d92cde3845a255d1ff913a2930df8ff56704b5
nachool <nachma.olivia1394@gmail.com>
2025-12-05 15:17:12 +0700

    setup interface DestinasiDetailSiswa.kt

--- app/src/main/java/com/example/myroomsatu/view/route/DestinasiDetailSiswa.kt
+++ app/src/main/java/com/example/myroomsatu/view/route/DestinasiDetailSiswa.kt
@@ -1,4 +1,11 @@
 package com.example.myroomsatu.view.route
 
-object DestinasiDetailSiswa {
+import com.example.myroomsatu.R
+
+object DestinasiDetailSiswa  : DestinasiNavigasi {
+    override val route = "detail_siswa"
+    override val titleRes = R.string.detail_siswa
+    const val itemIdArg = "idSiswa"
+    val routeWithArgs = "$route/{$itemIdArg}"
+
 }
