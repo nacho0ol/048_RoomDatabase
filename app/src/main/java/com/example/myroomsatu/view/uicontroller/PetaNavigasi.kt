68c5dc230f9c37b4bc5c803e2b0026247fd27766
nachool <nachma.olivia1394@gmail.com>
2025-12-05 15:46:20 +0700

    menambah destinasiDetialSiswa pada PetaNavigasi.kt

--- app/src/main/java/com/example/myroomsatu/view/uicontroller/PetaNavigasi.kt
+++ app/src/main/java/com/example/myroomsatu/view/uicontroller/PetaNavigasi.kt
@@ -4,13 +4,18 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
+import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+import com.example.myroomsatu.view.DetailSiswaScreen
 import com.example.myroomsatu.view.route.DestinasiEntry
 import com.example.myroomsatu.view.route.DestinasiHome
 import com.example.myroomsatu.view.EntrySiswaScreen
 import com.example.myroomsatu.view.HomeScreen
+import com.example.myroomsatu.view.route.DestinasiDetailSiswa
+import com.example.myroomsatu.view.route.DestinasiDetailSiswa.itemIdArg
 
 
 @Composable
@@ -36,5 +41,15 @@ fun HostNavigasi(
         composable(DestinasiEntry.route){
             EntrySiswaScreen(navigateBack = { navController.popBackStack()})
         }
+
+        composable(route = DestinasiDetailSiswa.routeWithArgs,
+            arguments = listOf(navArgument(itemIdArg){
+                type = NavType.IntType
+            })
+        ){
+            DetailSiswaScreen(
+                navigateBack = { navController.navigateUp()})
+        }
+
     }
 }
